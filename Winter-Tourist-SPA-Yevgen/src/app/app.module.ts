7f37c32ed3fd151201a5544ec7251ba857dd6ef4
Yevgen Kutsy <y.kutsy86@gmail.com>
2021-10-11 11:53:15 +0200

    basic structure and few components created

---
+++ Winter-Tourist-SPA-Yevgen/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutPageComponent,
+    NavigationComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
